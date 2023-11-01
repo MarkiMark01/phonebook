920f16ad5054e660571e1aca53ef10e4457c4320
MarkiMark01 <markishgroup@gmail.com>
2023-11-01 15:36:49 +0200

    Create App page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,105 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 
 function App() {
+  const [contacts, setContacts] = useState([]);
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+  const [filter, setFilter] = useState('');
+
+  // const searchRef = useRef();
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleNumberChange = (e) => {
+    setNumber(e.target.value);
+  };
+
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    addContacts();
+  };
+
+  const addContacts = () => {
+    if (name.trim() === '' || number.trim() === '') {
+      return;
+    }
+
+    if (contacts.some(contact => contact.name === name)) {
+      alert(`Contact with name ${name} already exists!`);
+      return;
+    }
+
+    const newContact = { name, number };
+    setContacts(prevContacts => [...prevContacts, newContact]);
+    setName('');
+    setNumber('');
+
+
+    localStorage.setItem('contacts', JSON.stringify([...contacts, newContact]));
+  };
+
+  function getFilteredContacts() {
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  }
+
+  const filteredContacts = getFilteredContacts();
+
+  const deleteContact = (index) => {
+    setContacts(prevContacts =>
+      prevContacts.filter((_, i) => i !== index)
+    );
+    const updatedContacts = contacts.filter((_, i) => i !== index);
+    localStorage.setItem('contacts', JSON.stringify(updatedContacts));
+  }
+
+  useEffect(() => {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts) {
+      setContacts(JSON.parse(savedContacts));
+    }
+  }, []);
+
+  // useEffect(() => {
+  //   searchRef.current.focus();
+  // }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Phonebook</h1>
+      <form onSubmit={handleFormSubmit}>
+        <div>
+          <h2>Name</h2>
+          <input type="text" value={name} onChange={handleNameChange} />
+          {/* <input type="text" value={name} onChange={handleNameChange} ref={searchRef} /> */}
+          <h2>Number</h2>
+          <input type="text" value={number} onChange={handleNumberChange} />
+        </div>
+        <button type="submit">Add contact</button>
+      </form>
+      <div>
+        <h2>Filter</h2>
+        <input type="text" value={filter} onChange={handleFilterChange} />
+      </div>
+      <h2>Contacts</h2>
+      <ul>
+        {filteredContacts.map((contact, id) => (
+          <li key={id}>
+            {contact.name}: {contact.number}
+            <button type="button" onClick={() => deleteContact(id)}>
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
